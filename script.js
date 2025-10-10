67bfc0f113171b142209a1c7f3c2dafa24504acc
Prabhacod <prabha29070607@gmail.com>
2025-10-10 15:31:08 +0530

    modified

---
+++ script.js
@@ -0,0 +1,35 @@
+/*let a=6;
+let b=a;
+a=4;
+console.log(b);  --> 6*/
+
+/*let object1 = {age:33};
+let object2 = object1;
+object1.age=22;
+console.log(object2.age);--> 22
+*/
+
+/*let a = true;
+let b = false;
+if(a||b){
+    console.log("executed");
+}*/
+
+//console.log(9<<2) -->36
+//console.log(9|5); --> 13
+//console.log(9^5); --> 12
+
+/*function sum(a,b){
+    return a+b;
+}
+console.log(sum(11,11));*/
+
+/*function sum(a,b){
+    console.log(a+b);
+}
+sum(11,10);*/
+
+const sum=(a,b,c)=>{
+    console.log(a+b+c);
+}
+sum(11,2,3);
